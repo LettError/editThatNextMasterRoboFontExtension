33f9b3fea7eeb8e23f97924076df38263e8c8a33
letterror <erik@letterror.com>
2021-10-11 14:14:03 +0200

    Make that version 1.4

--- buildExtension.py
+++ buildExtension.py
@@ -57,7 +57,7 @@ if resourcesPath:
     B.icon = imagePath
 
 # version of the extension
-B.version = '1.3'
+B.version = '1.4'
 
 # should the extension be launched at start-up?
 B.launchAtStartUp = True
